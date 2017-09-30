f21fd4e9370ca3dcca1b550c3110eeaba9e5ff3e
Saad Siddiqui <m.saadsid@outlook.com>
2017-09-30 13:33:43 +0500

    javascri: age calculator

---
+++ script.js
@@ -0,0 +1,27 @@
+// var website = "www.saad.com";
+//
+// alert("Hello User of "  + website);
+//
+// var fullName = prompt ("Please enter your Full Name");
+//
+// alert("Hello Mr " + fullName);
+
+// Second Test
+
+// var firstName = prompt ("Please enter your First Name here!");
+//
+// var lastName = prompt ("Please enter your Last Name here!");
+//
+// var age = prompt ("Please enter your Age here!");
+//
+// console.log("Your full name is " + firstName +" "+ lastName );
+//
+// console.log("Your are " + age + " years old");
+//
+// alert ("Your full name is " + firstName +" "+ lastName + " " +  "You are " + age + " years old" );
+
+var age = prompt("Please Enter your age here!");
+
+var totalAgeDays = age * 365;
+
+alert(totalAgeDays);
